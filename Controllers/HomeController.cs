c2e72d84de21a5015a784bb8a5d657d748b96abe
NiNeMarK <NiNeMarK@LOQ>
2024-08-27 23:12:45 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using WebContext.Data;
+
+namespace WebContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            using (var context = new AppDataContaxt())
+            {
+                // ใช้ SQL Query โดยตรง
+                string query = "SELECT * FROM PolicyMicroTwoHundred ";
+                var data = context.Database.SqlQuery<MyEntity>(query).ToList();
+
+                return View();
+            }
+
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Data/AppDataContaxt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebContext.Data
+{
+    public class AppDataContaxt : DbContext
+    {
+        public AppDataContaxt() : base("name=DataContext") { }
+
+        public DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public class MyEntity
+        {
+            public int ID { get; set; }
+            public int saleYear { get; set; }
+            public string PolicyID { get; set; }
+        }
+   
+}
